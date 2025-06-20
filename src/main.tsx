61f3f003f0c66edbb7dd27f64d7f9ab75510e830
Chelsey Machin <chelseymachin@gmail.com>
2025-06-19 21:39:16 -0700

    fix up css to center modal again

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@mantine/core/styles.css';
+import './App.css'
 import './index.css'
-import './App.css';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
